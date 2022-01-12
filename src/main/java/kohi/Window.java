93cf395a2ec671ed48e262aa7dca92011ef3304a
Marouène Boughoufa <maroueneboughoufa@gmail.com>
2022-01-12 13:10:03 +0100

    New commit

--- src/main/java/kohi/Window.java
+++ src/main/java/kohi/Window.java
@@ -2,10 +2,16 @@ package kohi;
 
 import org.lwjgl.Version;
 import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.glfw.GLFWVidMode;
 import org.lwjgl.opengl.GL;
+import org.lwjgl.system.MemoryStack;
 
+import java.nio.IntBuffer;
+
+import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
 import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryStack.stackPush;
 import static org.lwjgl.system.MemoryUtil.NULL;
 
 public class Window {
@@ -33,7 +39,14 @@ public class Window {
         System.out.println("Hello LWJGL " + Version.getVersion() + "!");
 
         init();
-        loop();
+        loop();// Free the window callbacks and destroy the window
+
+        glfwFreeCallbacks(glfwWindow);
+        glfwDestroyWindow(glfwWindow);
+
+        // Terminate GLFW and free the error callback
+        glfwTerminate();
+        glfwSetErrorCallback(null).free();
     }
 
     public void init() {
@@ -57,6 +70,26 @@ public class Window {
             throw new IllegalStateException("Failed to create the GLFW window.");
         }
 
+        try ( MemoryStack stack = stackPush() ) {
+            IntBuffer pWidth = stack.mallocInt(1); // int*
+            IntBuffer pHeight = stack.mallocInt(1); // int*
+
+            // Get the window size passed to glfwCreateWindow
+            glfwGetWindowSize(glfwWindow, pWidth, pHeight);
+
+            // Get the resolution of the primary monitor
+            GLFWVidMode vidMode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+
+            // Center the window
+            assert vidMode != null;
+            glfwSetWindowPos(
+                    glfwWindow,
+                    (vidMode.width() - pWidth.get(0)) / 2,
+                    (vidMode.height() - pHeight.get(0)) / 2
+            );
+        } // the stack frame is popped automatically
+
+
         // Make the OpenGL context current
         glfwMakeContextCurrent(glfwWindow);
         // Enable v-sync
@@ -64,22 +97,23 @@ public class Window {
 
         // Make the window visible
         glfwShowWindow(glfwWindow);
+    }
 
+    public void loop() {
         // This line is critical for LWJGL's interoperation with GLFW's
         // OpenGL context, or any context that is managed externally.
         // LWJGL detects the context that is current in the current thread,
         // creates the GLCapabilities instance and makes the OpenGL
         // bindings available for use.
         GL.createCapabilities();
-    }
 
-    public void loop() {
+        glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
+
         while (!glfwWindowShouldClose(glfwWindow)) {
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
+
             // Poll events
             glfwPollEvents();
-
-            glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
-            glClear(GL_COLOR_BUFFER_BIT);
 
             glfwSwapBuffers(glfwWindow);
         }
