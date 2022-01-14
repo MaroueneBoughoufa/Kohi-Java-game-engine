2e49a81b6c587e213bf9dadfa8b52b77e82eb611
Marouène Boughoufa <maroueneboughoufa@gmail.com>
2022-01-14 10:22:06 +0100

    New commit

--- src/main/java/kohi/Editor.java
+++ src/main/java/kohi/Editor.java
@@ -1,28 +1,10 @@
 package kohi;
 
-import static org.lwjgl.glfw.GLFW.*;
-
 public class Editor extends Scene{
-    private boolean changingScene = false;
-    private float timeToChangeScene = 2.0f;
-
     public Editor() {
-        System.out.println("Inside level editor scene");
     }
 
     @Override
     public void update(double dt) {
-        if (!changingScene && KeyListener.isKeyPressed(GLFW_KEY_SPACE)) {
-            changingScene = true;
-        }
-
-        if (changingScene && timeToChangeScene > 0) {
-            timeToChangeScene -= dt;
-            Window.get().r -= dt * 5.0f;
-            Window.get().g -= dt * 5.0f;
-            Window.get().b -= dt * 5.0f;
-        } else if (changingScene) {
-            Window.changeScene(1);
-        }
     }
 }
