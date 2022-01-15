cc13041f49ac777ad973816ba15bb13bbf210f18
Marouène Boughoufa <maroueneboughoufa@gmail.com>
2022-01-15 18:54:38 +0100

    Testing the Entity Component System

---
+++ src/main/java/components/Sprite.java
@@ -0,0 +1,15 @@
+package components;
+
+import kohi.Component;
+
+public class Sprite extends Component {
+    @Override
+    public void start() {
+        // Executes once the game starts
+    }
+
+    @Override
+    public void update(float dt) {
+        // Executes once every frame
+    }
+}
