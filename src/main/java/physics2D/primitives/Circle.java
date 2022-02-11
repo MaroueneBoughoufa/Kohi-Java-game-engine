773400d9556f82a560f67501ab235591c869b4c2
Marouène Boughoufa <maroueneboughoufa@gmail.com>
2022-02-11 22:48:54 +0100

    Added getter for the center

--- src/main/java/physics2D/primitives/Circle.java
+++ src/main/java/physics2D/primitives/Circle.java
@@ -1,9 +1,17 @@
 package physics2D.primitives;
 
+import org.joml.Vector2f;
+import physics2D.rigidbody.RigidBody2D;
+
 public class Circle {
     private float radius = 1.0f;
+    private RigidBody2D rigidBody = null;
 
     public float getRadius() {
         return radius;
+    }
+
+    public Vector2f getCenter() {
+        return rigidBody.getPosition();
     }
 }
