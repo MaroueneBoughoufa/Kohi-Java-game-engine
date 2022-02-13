ea71ae0d871193b807d7a737b26d801cf64b2087
Marouène Boughoufa <maroueneboughoufa@gmail.com>
2022-02-14 00:27:17 +0100

    Adding collision methods

--- src/main/java/physics2D/rigidbody/CollisionDetect2D.java
+++ src/main/java/physics2D/rigidbody/CollisionDetect2D.java
@@ -291,4 +291,106 @@ public class CollisionDetect2D {
 
         return circleToBox.lengthSquared() <= circle.getRadius() * circle.getRadius();
     }
+
+    //===============================================================
+    //  AABB vs. Shapes Methods
+    //===============================================================
+
+    public static boolean AABBinCircle(AABB box, Circle circle) {
+        return circleInAABB(circle, box);
+    }
+
+    public static boolean AABBinAABB(AABB b1, AABB b2) {
+        Vector2f[] axesToTest = {new Vector2f(0, 1), new Vector2f(1, 0)};
+        for (Vector2f axis : axesToTest) {
+            if (!overlapOnAxis(b1, b2, axis)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean AABBinBox2D(AABB b1, Box2D b2) {
+        Vector2f[] axesToTest = {
+                new Vector2f(0, 1), new Vector2f(1, 0),
+                new Vector2f(0, 1), new Vector2f(1, 0)
+        };
+
+        KMath.rotate(axesToTest[2], b2.getRigidBody().getRotation(), new Vector2f(0, 0));
+        KMath.rotate(axesToTest[3], b2.getRigidBody().getRotation(), new Vector2f(0, 0));
+
+        for (Vector2f axis : axesToTest) {
+            if (!overlapOnAxis(b1, b2, axis)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    //===============================================================
+    //  SAT Helpers
+    //===============================================================
+
+    private static boolean overlapOnAxis(AABB b1, AABB b2, Vector2f axis) {
+        Vector2f interval1 = getInterval(b1, axis);
+        Vector2f interval2 = getInterval(b2, axis);
+        return ((interval2.x <= interval1.y) && (interval1.x <= interval2.y));
+    }
+
+    private static boolean overlapOnAxis(AABB b1, Box2D b2, Vector2f axis) {
+        Vector2f interval1 = getInterval(b1, axis);
+        Vector2f interval2 = getInterval(b2, axis);
+        return ((interval2.x <= interval1.y) && (interval1.x <= interval2.y));
+    }
+
+    private static boolean overlapOnAxis(Box2D b1, Box2D b2, Vector2f axis) {
+        Vector2f interval1 = getInterval(b1, axis);
+        Vector2f interval2 = getInterval(b2, axis);
+        return ((interval2.x <= interval1.y) && (interval1.x <= interval2.y));
+    }
+
+    private static Vector2f getInterval(AABB rect, Vector2f axis) {
+        Vector2f result = new Vector2f(0, 0);
+
+        Vector2f min = rect.getMin();
+        Vector2f max = rect.getMax();
+
+        Vector2f[] vertices = {
+                new Vector2f(min.x, min.y), new Vector2f(min.x, max.y),
+                new Vector2f(max.x, min.y), new Vector2f(max.x, max.y)
+        };
+
+        result.x = axis.dot(vertices[0]);
+        result.y = result.x;
+        for (int i = 0; i < 4; i++) {
+            float projection = axis.dot(vertices[i]);
+            if (projection < result.x) {
+                result.x = projection;
+            }
+            if (projection > result.y) {
+                result.y = projection;
+            }
+        }
+        return result;
+    }
+
+    private static Vector2f getInterval(Box2D rect, Vector2f axis) {
+        Vector2f result = new Vector2f(0, 0);
+
+        Vector2f[] vertices = rect.getVertices();
+
+        result.x = axis.dot(vertices[0]);
+        result.y = result.x;
+        for (int i = 0; i < 4; i++) {
+            float projection = axis.dot(vertices[i]);
+            if (projection < result.x) {
+                result.x = projection;
+            }
+            if (projection > result.y) {
+                result.y = projection;
+            }
+        }
+        return result;
+    }
+
 }
