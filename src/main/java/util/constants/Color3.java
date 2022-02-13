f427ae3593b2262012d0cb3520ee6be77cb14dd6
Marouène Boughoufa <maroueneboughoufa@gmail.com>
2022-02-14 00:28:03 +0100

    Added constants for colors

--- src/main/java/util/constants/Color3.java
+++ src/main/java/util/constants/Color3.java
@@ -6,4 +6,7 @@ public class Color3 {
     public static final Vector3f RED = new Vector3f(1, 0, 0);
     public static final Vector3f GREEN = new Vector3f(0, 1, 0);
     public static final Vector3f BLUE = new Vector3f(0, 0, 1);
+    public static final Vector3f BLACK = new Vector3f(0, 0, 0);
+    public static final Vector3f WHITE = new Vector3f(1, 1, 1);
+    public static final Vector3f GRAY = new Vector3f(0.5f, 0.5f, 0.5f);
 }
--- src/main/java/util/constants/Color4.java
+++
@@ -1,9 +0,0 @@
-package util.constants;
-
-import org.joml.Vector4f;
-
-public class Color4 {
-    public static final Vector4f RED = new Vector4f(1, 0, 0, 1);
-    public static final Vector4f GREEN = new Vector4f(0, 1, 0, 1);
-    public static final Vector4f BLUE = new Vector4f(0, 0, 1, 1);
-}
